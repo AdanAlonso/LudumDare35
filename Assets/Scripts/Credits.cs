4b4c39a9c800647a81a9c5ea41732788f8e330d7
Adan Alonso <adan.alonso.s@gmail.com>
2016-04-16 20:15:06 +0100

    menús limpios

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public string menu = "MainMenu";
+
+	public void Menu() {
+		SceneManager.LoadScene (menu);
+	}
+}
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,9 +5,14 @@ using System.Collections;
 public class Menu : MonoBehaviour {
 
 	public string level = "Level";
+	public string credits = "Credits";
 
 	public void Play() {
 		SceneManager.LoadScene(level);
+	}
+
+	public void Credits() {
+		SceneManager.LoadScene(credits);
 	}
 
 	public void Quit() {
