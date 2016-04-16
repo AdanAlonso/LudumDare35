437f5425178fd7002f2aec4fa3def46176b25404
Adan Alonso <adan.alonso.s@gmail.com>
2016-04-16 12:41:18 +0100

    prototipo salto

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public Transform target;
+	public bool followX;
+	public bool followY;
+
+	void Update(){
+		Vector3 newPosition = target.position;
+		if (!followX) {
+			newPosition.x = 0;
+		}
+		if (!followY) {
+			newPosition.y = 0;
+		}
+		newPosition.z = transform.position.z;
+		transform.position = newPosition;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public KeyCode leftButton = KeyCode.LeftArrow;
+	public KeyCode rightButton = KeyCode.RightArrow;
+	public float speed = 10f;
+	public KeyCode jumpButton = KeyCode.Space;
+	public float jumpPower = 5f;
+
+	Rigidbody2D rb;
+	bool jumping = false;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.right * h, Time.deltaTime * speed);
+
+		if (!jumping && Input.GetKeyDown (jumpButton)) {
+			jumping = true;
+			rb.AddForce(Vector3.up * jumpPower);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.CompareTag("Ground") && other.transform.position.y < transform.position.y) {
+			jumping = false;
+		}
+	}
+}
