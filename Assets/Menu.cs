4de638cacabf2412dbef01d50a3cb35774fa3f92
Adan Alonso <adan.alonso.s@gmail.com>
2016-04-16 10:16:01 +0100

    menú principal

---
+++ Assets/Menu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public string level = "Level";
+
+	public void Play() {
+		SceneManager.LoadScene(level);
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+
+}
