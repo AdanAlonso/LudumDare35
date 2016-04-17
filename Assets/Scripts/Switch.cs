9d322c78ac1abf16bc9c683665e1207fcc1be1e9
Adan Alonso <adan.alonso.s@gmail.com>
2016-04-17 12:24:18 +0100

    puertas con botones

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+	public Transform door;
+	public Transform switchTarget;
+	public Transform doorTarget;
+	public float switchTime = 0.5f;
+	public float delay = 0.5f;
+	public float doorTime = 0.5f;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Player p = other.GetComponentInParent<Player>();
+		if (p != null && p.state == Player.States.Solid) {
+			StartCoroutine(PressSwitch ());
+		}
+	}
+
+	IEnumerator PressSwitch() {
+		float counter = 0;
+		Vector3 origin = transform.position;
+		while (counter < switchTime) {
+			yield return 0;
+			transform.position = Vector3.Lerp (origin, switchTarget.position, counter/switchTime);
+			counter += Time.deltaTime;
+		}
+		yield return new WaitForSeconds (delay);
+		StartCoroutine (OpenDoor());
+	}
+
+	IEnumerator OpenDoor() {
+		float counter = 0;
+		Vector3 origin = door.position;
+		while (counter < doorTime) {
+			yield return 0;
+			door.position = Vector3.Lerp (origin, doorTarget.position, counter/switchTime);
+			counter += Time.deltaTime;
+		}
+		yield return 0;
+	}
+}
